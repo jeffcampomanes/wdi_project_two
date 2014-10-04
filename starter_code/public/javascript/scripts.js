f196a2fb25a9359aa25b607c262fd99d0b954bac
jeffcampomanes <jeffcampomanes@me.com>
2014-10-04 17:00:28 -0400

    delete works, edit still does not work.

--- starter_code/public/javascript/scripts.js
+++ starter_code/public/javascript/scripts.js
@@ -4,12 +4,13 @@ console.log("testing html page");
 /////////////////////////////////////////////////////////////////
 // loading contact list from allContacts function
 allContacts();
+
 // loading category list from displayCategories function
 displayCategory();
 
 
 // submit button on form
-var button = $('button#submit');
+var button = $('i#submit');
 	button.on("click", function(event) {
 	event.preventDefault();
 			console.log("button click");
@@ -30,10 +31,10 @@ $.ajax({
 		data:{name: name, age: age, address: address, phone_number: phone_number, category_id: category_id}
 			}).done(function(data){
 			console.log(data);
-			ul_contacts.append("<li>" +name)
+			ul_contacts.append("<li>" + name + "<button class='edit'>Edit</button>")
 			
 			// may want to use this for /contacts/:id//	
-			// ul_contacts.append("<li>" + "Name: " + name + "<br>" + "Age: " + age + "<br>" + "Address: " + address + "<br>" + "Phone: " + phone_number +"<br>"+ "Category ID: " + category_id)
+			// ul_contacts.append("<li>" + "Name: " + name + "<br>" + "Age: " + age + "<br>" + "Address: " + address + "<br>" + "Phone: " + phone_number +"<br>"+ "Category ID: " + category_id+ "<button class='edit'>Edit</button>")
 		})
 });
 
@@ -47,11 +48,11 @@ $.ajax({
 				console.log(data);
 			
 				var categories = data;
-				var ul_categories = $('ul');
+				var h4_categories = $('.all_categories');
 				
 				for(i = 0; i < categories.length; i++){
 					
-					// ul_categories.append('<li id="categories">' +categories[i]["name"]+ '</li>');	
+					h4_categories.append('<li id="categories">' +categories[i]["name"]+ '</li>');	
 					var contactInfo_categoryID = '<option value="' + categories[i].id + '">' + categories[i]["name"] + '</option>'
 					$('select').append(contactInfo_categoryID); 
 			}
@@ -67,16 +68,102 @@ function allContacts() {
 				var contacts = data;
 				console.log(data);
 	
-				var ul = $('.all_contacts');
+				var ul_contacts = $('.all_contacts');
 				var h2 = $('.all_header');
 			
 				h2.append("All Contacts")
 					for (i=0; i < contacts.length; i++) {
-					ul.append("<li id='" + contacts[i]["id"] + "'>" +  contacts[i]["name"] + "</li>")
+					ul_contacts.append("<li id='" + contacts[i]["id"] + "'>" +  contacts[i]["name"] + "<button class='edit'>Edit</button>")				
 				}
+					editButton();
+
 		});
 };
 
+// edit button, new form appears, how to change categories and make edit button dissapear upon click??
+function editButton() {
+			var $editButton = $("button.edit");
+			$editButton.on("click", function(){
+				console.log("edit button has been clicked")
+				$(this).parent().append("<br>" + "<input id='newName' placeholder='Name' </input> <br> <input id='newAge' placeholder='Age' </input> <br> <input id='newAddress' placeholder='Address' </input> <br> <input id='newPhoneNumber' placeholder='Phone Number' </input> <button class='save'>Save</button> <button class='delete'>Delete</button></li>");
+				
+				saveButton();
+				deleteButton();
+		});
+};
+
+// save button
+function saveButton(){
+	var $saveButton = $("button.save");
+	$saveButton.on("click", function(){
+		console.log("save button has been clicked");
+		
+			var id = $(this).parent().attr("id");
+			
+			var $newNameInput = $("input#newName");
+			var $newAgeInput = $("input#newAge");
+			var $newPhoneNumberInput = $("input#newPhoneNumber")
+			
+			var newName = $newNameInput.val();
+			var newAge = $newAgeInput.val();
+			var newPhoneNumber = $newPhoneNumberInput.val();
+
+
+		putRequestContact(newName, newAge, newPhoneNumber);
+	});
+};
+
+// delete button
+function deleteButton() {
+	var deleteButton = $('.delete');
+	deleteButton.on("click", function() {
+		console.log('Delete button working');
+		var name = $('#name').val();
+		var age = $('#age').val();
+		var address = $('#address').val();
+		var phone_number = $('#phone_number').val();
+		var category_id = $('#category_id').val();
+		
+		var id = $(this).parent().attr("id");
+		var li = $('#'+id);
+
+		li.remove();
+
+	$.ajax({
+		url:'/contacts/'+id,
+		type: 'DELETE',
+		data: {name: name, age: age, address: address, phone_number: phone_number, category_id: category_id, id: id }	
+			}).done(function(response){
+			console.log(response);
+		})
+	});
+};
+
+// put request for contact. THIS IS A FUCKING MESS
+function putRequestContact(name, age, address, phone_number){
+	$.ajax({
+		type: "PUT",
+		url: "/contacts",
+		data: {name: name, age: age, address: address, phone_number: phone_number}
+		}).done(function(data){
+			console.log(data);
+				var contacts = JSON(data);
+				var li = contacts["id"];
+				$('li').html("<li>" + 'Name: '+ newName + " <br> " + 'Age: ' +newAge + phone_number );
+		});
+	}
+
+// function allCategories() {
+// 		var leftColumn = $('.column_header1');
+// 		leftColumn.append(category[0]["name"]);
+
+// 		var middleColumn = $('.column_header2');
+// 		middleColumn.append(category[1]["name"]);
+
+// 		var rightColumn = $('.column_header3');
+// 		rightColumn.append(category[2]["name"]);
+// 		}
+// allCategories();
 
 
 
