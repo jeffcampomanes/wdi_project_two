2a5789fc5d7b0ee4a932f6fb7d8aca3c29b94c53
jeffcampomanes <jeffcampomanes@me.com>
2014-10-05 16:30:46 -0400

    sorting working, staritng randomuser api

--- starter_code/public/javascript/scripts.js
+++ starter_code/public/javascript/scripts.js
@@ -31,15 +31,19 @@ $.ajax({
 		data:{name: name, age: age, address: address, phone_number: phone_number, category_id: category_id}
 			}).done(function(data){
 			console.log(data);
-			ul_contacts.append("<li>" + name + "<button class='edit'>Edit</button>")
 			
-			// may want to use this for /contacts/:id//	
+			ul_contacts.append("<li>" + name + "<button class='edit'>Edit</button>");
+
+			editButton();
+			deleteButton();
+			
 			// ul_contacts.append("<li>" + "Name: " + name + "<br>" + "Age: " + age + "<br>" + "Address: " + address + "<br>" + "Phone: " + phone_number +"<br>"+ "Category ID: " + category_id+ "<button class='edit'>Edit</button>")
 		})
+		window.location.reload();
 });
 
 
-// display categories on page
+// display all categories on page and from select dropdown
 function displayCategory() {
 $.ajax({
 			type:"GET",
@@ -52,12 +56,58 @@ $.ajax({
 				
 				for(i = 0; i < categories.length; i++){
 					
-					h4_categories.append('<li id="categories">' +categories[i]["name"]+ '</li>');	
+					// h4_categories.append('<li id="categories">' +categories[i]["name"]+ '</li>');	
 					var contactInfo_categoryID = '<option value="' + categories[i].id + '">' + categories[i]["name"] + '</option>'
 					$('select').append(contactInfo_categoryID); 
 			}
 	});
 };
+
+
+
+var categories = [{"id":1,"name":"friends"},{"id":2,"name":"family"},{"id":3,"name":"co_worker"}];
+
+var leftColumn = $('.column_header1');
+	contactsInCategory("friends");
+
+var middleColumn = $('.column_header2');
+	contactsInCategory("family");
+
+var rightColumn = $('.column_header3');
+	contactsInCategory("co_worker");
+
+//sorting contacts and displaying via specific category
+function contactsInCategory(category) {
+		for (i = 0; i < categories.length; i++){
+			if (category == categories[i]["name"]) {
+			category_id = categories[i]["id"];		
+			};
+};
+
+$.ajax({
+		url: '/categories/' + category_id,
+		type: 'GET'
+		}).done(function(data){
+		
+		var contacts = data["contacts"];
+			console.log(contacts);
+		
+		var ul = $("#" + category);
+		var ul_friends = $('#friends');
+		var ul_family = $('#family');
+		var ul_co_worker = $('#co_worker');
+
+
+		for (i = 0; i < contacts.length; i++) {
+			var contactName = contacts[i]["name"];
+			var contactAge = contacts[i]["age"];
+			var contactAddress = contacts[i]["address"];
+			var contactPhoneNumber = contacts[i]["phone_number"];
+			ul.append("<li>" + contactName + " </li>");
+		};
+	});
+};
+
 
 // displaying all contacts, function called at the top upon loading the page
 function allContacts() {
@@ -70,15 +120,18 @@ function allContacts() {
 	
 				var ul_contacts = $('.all_contacts');
 				var h2 = $('.all_header');
+
 			
 				h2.append("All Contacts")
 					for (i=0; i < contacts.length; i++) {
-					ul_contacts.append("<li id='" + contacts[i]["id"] + "'>" +  contacts[i]["name"] + "<button class='edit'>Edit</button>")				
+					ul_contacts.append("<li id='" + contacts[i]["id"] + "'>" + "Name: " +  contacts[i]["name"] +"<br>"+ "Age: " + contacts[i]["age"]+ "<br>" + "Phone Number: " + contacts[i]["phone_number"] + "<br>" + "Address: "+ contacts[i]["address"] + "<br>" + "<button class='edit'>Edit</button>")				
 				}
 					editButton();
-
+					saveButton();
+					deleteButton();
 		});
 };
+
 
 // edit button, new form appears, how to change categories and make edit button dissapear upon click??
 function editButton() {
@@ -92,7 +145,9 @@ function editButton() {
 		});
 };
 
-// save button
+/////////////////////////////////////////////////////////////////
+
+// save button, click working, WORK IN PROGRESS
 function saveButton(){
 	var $saveButton = $("button.save");
 	$saveButton.on("click", function(){
@@ -112,12 +167,16 @@ function saveButton(){
 		putRequestContact(newName, newAge, newPhoneNumber);
 	});
 };
+// save button, click working, WORK IN PROGRESS
+/////////////////////////////////////////////////////////////////
+
 
 // delete button
 function deleteButton() {
 	var deleteButton = $('.delete');
 	deleteButton.on("click", function() {
 		console.log('Delete button working');
+	
 		var name = $('#name').val();
 		var age = $('#age').val();
 		var address = $('#address').val();
@@ -130,7 +189,7 @@ function deleteButton() {
 		li.remove();
 
 	$.ajax({
-		url:'/contacts/'+id,
+		url:"/contacts/"+id,
 		type: 'DELETE',
 		data: {name: name, age: age, address: address, phone_number: phone_number, category_id: category_id, id: id }	
 			}).done(function(response){
@@ -139,11 +198,16 @@ function deleteButton() {
 	});
 };
 
-// put request for contact. THIS IS A FUCKING MESS
+/////////////////////////////////////////////////////////////////
+
+// put request for contact. WORK IN PROGRESS. THIS IS A FUCKING MESS
 function putRequestContact(name, age, address, phone_number){
-	$.ajax({
+
+		var id = $(this).parent().attr("id");
+
+		$.ajax({
 		type: "PUT",
-		url: "/contacts",
+		url: "/contacts/"+id,
 		data: {name: name, age: age, address: address, phone_number: phone_number}
 		}).done(function(data){
 			console.log(data);
@@ -151,7 +215,22 @@ function putRequestContact(name, age, address, phone_number){
 				var li = contacts["id"];
 				$('li').html("<li>" + 'Name: '+ newName + " <br> " + 'Age: ' +newAge + phone_number );
 		});
-	}
+	};
+
+/////////////////////////////////////////////////////////////////
+
+
+
+// sorting 	
+// function sortingContacts(category){
+
+// 	if(category.friends == 1)
+// 		console.log("friends");
+// 	else if (category.family == 2)
+// 		console.log("family");
+// 	else return ("co_workers");
+// };
+
 
 // function allCategories() {
 // 		var leftColumn = $('.column_header1');
